cc3694050ffa075c6a36db944f93ace7d58e50f3
GraxCode <grax@graxco.de>
2020-04-29 16:52:39 +0200

    field signatures

--- src/main/java/me/nov/dalvikgate/transform/fields/FieldTransfomer.java
+++ src/main/java/me/nov/dalvikgate/transform/fields/FieldTransfomer.java
@@ -1,7 +1,13 @@
 package me.nov.dalvikgate.transform.fields;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.jf.dexlib2.dexbacked.DexBackedAnnotation;
 import org.jf.dexlib2.dexbacked.DexBackedField;
+import org.jf.dexlib2.dexbacked.value.DexBackedArrayEncodedValue;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.FieldNode;
 
 import me.nov.dalvikgate.transform.ITransformer;
@@ -24,7 +30,24 @@ public class FieldTransfomer implements ITransformer<FieldNode>, Opcodes {
       flags |= ACC_SYNTHETIC;
     }
     fn = new FieldNode(flags, name, field.getType(), null, ValueBridge.toObject(field.getInitialValue()));
-    // TODO annotations
+    Set<? extends DexBackedAnnotation> annotations = field.getAnnotations();
+    if (annotations != null) {
+      for (DexBackedAnnotation anno : annotations) {
+        String type = anno.getType();
+        if ("Ldalvik/annotation/Signature;".equals(type)) {
+          // Signatures stored in an array of strings.
+          // Concatting all items will yield the full signature.
+          fn.signature = ValueBridge.arrayToString((DexBackedArrayEncodedValue) anno.getElements().iterator().next().getValue());
+        } else if (type != null) {
+          // TODO normal annotations
+          // if(anno.getVisibility() == ...)
+          if (fn.visibleAnnotations == null) {
+            fn.visibleAnnotations = new ArrayList<>();
+          }
+          fn.visibleAnnotations.add(new AnnotationNode(type));
+        }
+      }
+    }
   }
 
   @Override
