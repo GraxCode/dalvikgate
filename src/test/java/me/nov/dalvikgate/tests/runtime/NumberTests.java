741386b7b0f52e1d4a985264bdd8bdd1c95bf03e
GraxCode <grax@graxco.de>
2020-05-05 22:38:42 +0200

    add more test cases

--- src/test/java/me/nov/dalvikgate/tests/runtime/NumberTests.java
+++ src/test/java/me/nov/dalvikgate/tests/runtime/NumberTests.java
@@ -13,20 +13,56 @@ import me.nov.dalvikgate.tests.utils.Factory;
 class NumberTests implements Opcodes {
 
   @Test
-  void intLdc() {
+  void intConst32() {
     MutableMethodImplementation mmi = new MutableMethodImplementation(1);
     mmi.addInstruction(new BuilderInstruction31i(Opcode.CONST, 0, 1234));
     mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
 
-    assertEquals(1235, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.INT_TYPE), mmi)));
+    assertEquals(1234, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.INT_TYPE), mmi)));
   }
 
   @Test
-  void floatLdc() {
+  void floatConst32() {
+    // unusual float construction
     MutableMethodImplementation mmi = new MutableMethodImplementation(1);
     mmi.addInstruction(new BuilderInstruction31i(Opcode.CONST, 0, Float.floatToIntBits(1.2f)));
     mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
 
     assertEquals(1.2f, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.FLOAT_TYPE), mmi)));
+  }
+
+  @Test
+  void longConst64() {
+    MutableMethodImplementation mmi = new MutableMethodImplementation(2);
+    mmi.addInstruction(new BuilderInstruction51l(Opcode.CONST_WIDE, 0, 423456L));
+    mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
+
+    assertEquals(423456L, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.LONG_TYPE), mmi)));
+  }
+
+  @Test
+  void doubleConst64() {
+    // unusual double construction
+    MutableMethodImplementation mmi = new MutableMethodImplementation(2);
+    mmi.addInstruction(new BuilderInstruction51l(Opcode.CONST_WIDE, 0, Double.doubleToLongBits(363412)));
+    mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
+    assertEquals(363412, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.DOUBLE_TYPE), mmi)));
+  }
+
+  @Test
+  void floatConstHigh16() {
+    MutableMethodImplementation mmi = new MutableMethodImplementation(1);
+    mmi.addInstruction(new BuilderInstruction21ih(Opcode.CONST_HIGH16, 0, Float.floatToIntBits(21.3f)));
+    mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
+
+    assertEquals(21.3f, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.FLOAT_TYPE), mmi)));
+  }
+
+  @Test
+  void doubleConstWideHigh16() {
+    MutableMethodImplementation mmi = new MutableMethodImplementation(2);
+    mmi.addInstruction(new BuilderInstruction21lh(Opcode.CONST_WIDE_HIGH16, 0, Double.doubleToLongBits(363417)));
+    mmi.addInstruction(new BuilderInstruction11x(Opcode.RETURN, 0));
+    assertEquals(363417, Factory.executeMethodAtRuntime(Factory.runDexToASM(Type.getMethodType(Type.DOUBLE_TYPE), mmi)));
   }
 }
