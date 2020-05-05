122b77058d80bfc68819b4a81e53c4e49008d5c1
GraxCode <grax@graxco.de>
2020-05-05 22:40:17 +0200

    javadoc

--- src/main/java/me/nov/dalvikgate/transform/instructions/IUnresolvedInstruction.java
+++ src/main/java/me/nov/dalvikgate/transform/instructions/IUnresolvedInstruction.java
@@ -4,12 +4,12 @@ import org.objectweb.asm.Type;
 
 public interface IUnresolvedInstruction {
   /**
-   * Validate the opcode is set.
+   * Validate the opcode is set. Should throw an exception if setType hasn't been invoked.
    */
   void validate();
 
   /**
-   * Update the instruction's type to determine its opcode.
+   * Update the instruction's type to determine its opcode and value.
    *
    * @param type Type discovered by analyzer.
    */
